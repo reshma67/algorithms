b022f4e226bd4caf7641a5b743a0ee39745a9857
Reshma R <r.reshma67@gmail.com>
2013-04-02 17:50:24 +0530

    initial version of checking if number is prime

---
+++ isprime.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int isprime(int n)
+{
+    int sqroot,f;
+    sqroot=sqrt(n);
+    for(f=2;f<=sqroot;f++)
+    {
+        if((n%f)==0){
+        return 0;
+        }
+        return 1;
+
+    }
+
+
+}
